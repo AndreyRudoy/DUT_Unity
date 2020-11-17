bdc3338eac0a373ed095746e1b571801b8199202
Andrew <andrey.rudy1@gmail.com>
2020-11-18 00:01:23 +0200

    Unity2D
    Unity2D

---
+++ Assets/Scripts/Player/CoinPicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinPicker : MonoBehaviour
+{
+
+    [SerializeField] private int manyCoins;
+    [SerializeField] private TMP_Text mashCoin;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<Player_controller>()._coinScore += manyCoins;
+        Destroy(gameObject);
+        mashCoin.text = "X" + collision.gameObject.GetComponent<Player_controller>()._coinScore;
+    }
+}
